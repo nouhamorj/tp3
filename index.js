28b1599eee0c2b787b3c2b19830a4b372c957a2a
nouhamorj <nouhamorjene069@gmail.com>
2024-01-30 10:11:00 +0100

    test

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const taskSchema = require('./taskSchema');
+const taskResolver = require('./taskResolver');
+const app = express();
+    app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: taskSchema,
+        rootValue: taskResolver,
+        graphiql: true,
+    })
+);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
